c750b950d2a73cabed69667bab261445dc6bbace
YuriBortolato <yurigbort@gmail.com>
2025-09-09 21:20:39 -0300

    refactor: reconfigura código para melhorar estrutura

--- include/header.php
+++ include/header.php
@@ -3,12 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>tcc</title>
 
     <link rel="stylesheet" href="../assets/bootstrap.min.css">
-    <link rel="stylesheet" href="../assets/bootstrap.min.css.map">
-
-
 </head>
 <body>
     
