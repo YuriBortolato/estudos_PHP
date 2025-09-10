26a62a8b49dd5b9f7f2203ef9eb467d66008a511
YuriBortolato <yurigbort@gmail.com>
2025-09-10 11:03:29 -0300

    feat: adicionar validação de informações

--- pessoa/save.php
+++ pessoa/save.php
@@ -2,6 +2,17 @@
     //print_r($_POST);
 
     include '../config/db.php';
+
+    function validarCPF($cpf) {
+        $cpf = preg_replace('/[^0-9]/', '', $cpf);
+        if (strlen($cpf) != 11 || preg_match('/(\d)\1{10}/', $cpf)) return false;
+        for ($t = 9; $t < 11; $t++) {
+            for ($d = 0, $c = 0; $c < $t; $c++) $d += $cpf[$c] * (($t + 1) - $c);
+            $d = ((10 * $d) % 11) % 10;
+            if ($cpf[$c] != $d) return false;
+        }
+        return true;
+    }
     
     if($_SERVER['REQUEST_METHOD'] === 'POST'){
 
@@ -13,6 +24,10 @@
         $endereco = $_POST['endereco'];
         $cidade = $_POST['cidade'];
         $estado = $_POST['estado'];
+
+        if(!validarCPF($cpf)){
+            die('CPF invalido');
+        }
 
         // $sql = "INSERT INTO pessoa (nome, email, cpf, cep, endereco, cidade, estado)
         //  VALUES
