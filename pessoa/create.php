978e3025ac3b28c306e5771fac88fcc6331c417a
YuriBortolato <yurigbort@gmail.com>
2025-09-09 21:23:43 -0300

    feat: adiciona novas funções ao cadastro corrigindo falhas anteriores

--- pessoa/create.php
+++ pessoa/create.php
@@ -1,17 +1,43 @@
 <?php 
-include '../config/db.php';
-include '../include/header.php';
+    include '../config/db.php';
+    include '../include/header.php'; 
 ?>
 
 <div class="container mt-4">
-    <h2> Cadastrar pessoas  </h2>
-    <form action="save.php" id="fromPessoa">
+    <h2>Atualizar Pessoa</h2>
+    <form action="save.php" method="POST" id="formPessoa">
         <div class="mb-2">
-            <label>Nome: </label>
-            <input type="text" name="form-control" required>
+            <label>Nome:</label>
+            <input type="text" name="nome" class="form-control" required>
         </div>
-        <button class="btn btn-sucess" type="submit">Salvar</button>
+        <div class="mb-2">
+            <label>CPF:</label>
+            <input type="text" name="cpf" class="form-control" required>
+        </div>
+        <div class="mb-2">
+            <label>Email:</label>
+            <input type="email" name="email" class="form-control" required>
+        </div>
+        <div class="mb-2">
+            <label>CEP:</label>
+            <input type="text" name="cep" class="form-control" required>
+        </div>
+        <div class="mb-2">
+            <label>Endereço:</label>
+            <input type="text" name="endereco" class="form-control" required>
+        </div>
+        <div class="mb-2">
+            <label>Cidade:</label>
+            <input type="text" name="cidade" class="form-control" required>
+        </div>
+        <div class="mb-2">
+            <label>Estado:</label>
+            <input type="text" name="estado" class="form-control" required>
+        </div>
+        <button class="btn btn-success" type="submit">Salvar</button>
     </form>
 </div>
 
-<?php include '../include/footer.php';  ?>
+<?php 
+    include '../include/footer.php'; 
+?>
