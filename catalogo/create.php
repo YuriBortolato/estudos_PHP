2a2ba4a50d71e70aa532797095aec0551ef35abc
YuriBortolato <yurigbort@gmail.com>
2025-09-12 10:13:10 -0300

    feat: criar funcionalidade de cadastro

---
+++ catalogo/create.php
@@ -0,0 +1,26 @@
+<?php 
+    include '../config/db.php';
+    include '../include/header.php'; 
+?>
+
+<div class="container mt-4">
+    <h2>Cadastrar Catálogo</h2>
+    <form action="save.php" method="POST" id="formCatalogo">
+        <div class="mb-2">
+            <label>Nome:</label>
+            <input type="text" name="nome" class="form-control" required>
+        </div>
+        <div class="mb-2">
+            <label>Descrição:</label>
+            <input type="text" name="descricao" class="form-control" required>
+        </div>
+        <button class="btn btn-success" type="submit">Salvar</button>
+    </form>
+</div>
+
+<script src="https://jsuites.net/v5/jsuites.js"></script>
+<script src="../js/script.js"></script>
+
+<?php 
+    include '../include/footer.php'; 
+?>
---
+++ produto/create.php
@@ -0,0 +1,39 @@
+<?php 
+    include '../config/db.php';
+    include '../include/header.php'; 
+?>
+
+<div class="container mt-4">
+    <h2>Atualizar Produto</h2>
+    <form action="save.php" method="POST" id="formProduto">
+        <div class="mb-2">
+            <label>Código:</label>
+            <input type="text" name="codigo" class="form-control" required>
+        </div>
+        <div class="mb-2">
+            <label>Nome:</label>
+            <input type="text" name="nome" class="form-control" required>
+        </div>
+        <div class="mb-2">
+            <label>Descrição:</label>
+            <input type="text"  name="descricao" class="form-control" required>
+        </div>
+        <div class="mb-2">
+            <label>Preço:</label>
+            <input type="number" step="0.01" name="preco" class="form-control" required>
+        </div>
+        <div class="mb-2">
+            <label>Quantidade:</label>
+            <input type="number" name="quantidade" class="form-control" required>
+        </div>
+        <button class="btn btn-success" type="submit">Salvar</button>
+    </form>
+</div>
+
+
+<script src="https://jsuites.net/v5/jsuites.js"></script>
+<script src="../js/script.js"></script>
+
+<?php 
+    include '../include/footer.php'; 
+?>
