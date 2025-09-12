9c4539642aafeb6130686484d78858e5612f44e4
YuriBortolato <yurigbort@gmail.com>
2025-09-12 10:16:17 -0300

    fix: corrigir erro na exclusão de usuário

--- usuario/delete.php
+++ usuario/delete.php
@@ -2,7 +2,7 @@
     include '../config/db.php';
     if(isset($_GET['id'])){
         $id = $_GET['id'];
-        $sql = $conn->prepare("DELETE FROM pessoa WHERE id = ?");
+        $sql = $conn->prepare("DELETE FROM usuario WHERE id = ?");
         $sql->bind_param('i', $id);
         $sql->execute();
         $sql->close();
